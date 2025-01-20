ec4fc398a4b9c0a1fcba14bfc038b216879e715a
LauraM101 <laura.medina@correo.unimet.edu.ve>
2025-01-20 17:30:01 -0400

    Add files via upload

--- laura_m_tarea_4.py
+++ laura_m_tarea_4.py
@@ -1,19 +1,26 @@
 print("Hola el día de hoy vamos a calcular los años bisiestos de 1900 a 2199 ")
-año= (input("Indique el año de 1900 a 2199 "))
-año_usario= int(año)
+año= input("Indique el año de 1900 a 2199 ")
 
-if año == año_usario:
+while not año.isnumeric() or int(año) < 0:
+    año= input("tiene que ser un año de 1900 a 2199")
+    
+año= int(año) 
+
+while not 1900 <= año <=2199 :
+    año= input("este año no se encuentra en el rango. Ingrese de nuevo el número: ")
+
+    año= int(año)
     año_limite= int(1900)
     año_mayor= int(2199)
     n=0
 
-    while año_usario>= año_limite and año_usario<= año_mayor:
-        if año_usario%4 ==0:
-            if año_usario%100 == 0 and año_usario%400 !=0:
+    while año_limite <= año <= año_mayor:
+        if año_limite%4 ==0:
+            if año_limite%100 == 0 and año_limite%400 !=0:
                 pass
             else:
-                n+=1
+                 n+=1
         año_limite+=1
+    print(f"estos son los años bisiesto: {n}")
 
-else:
-    print("tiene que ser un año")
+
